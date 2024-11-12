40f5fe00a422a8ab97425b17ea1558a93bdab95a
suryasingh <singhsurya0333@gmail.com>
2024-11-12 18:50:01 +0530

    check 1

--- apps/user-app/component/BalanceCard.tsx
+++ apps/user-app/component/BalanceCard.tsx
@@ -23,7 +23,7 @@ export const BalanceCard = ({amount, locked} : {
         </div>
         <div className="flex justify-between border-b border-slate-300 py-2">
             <div>
-                Total Balance
+                Total Balance in your wallet
             </div>
             <div>
                 {(locked + amount) / 100} INR
