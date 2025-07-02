a58ec704a377cb87dc9155869ca7a8caa6192b9b
suryasingh <singhsurya0333@gmail.com>
2025-07-02 20:11:47 +0530

    fix

--- apps/user-app/app/lib/actions/createOnrampTransactions.ts
+++ apps/user-app/app/lib/actions/createOnrampTransactions.ts
@@ -5,12 +5,12 @@ import { authOptions } from "../auth";
 
 export async function onCreateRamptransaction(provider: string, amount: number){
 
-    // never share the id as a prop because somone can share alternate id as a prop and make a false call , therefore always try to extract it from the session
+    // never share the id as a prop because someone can share alternate id as a prop and make a false call , therefore always try to extract it from the session
     const session = await getServerSession(authOptions);
 
     if(!session?.user || !session.user?.id)
         return {
-            message: "Unauthenicated request"    
+            message: "Unauthenicated request"
         }
 
     const token = (Math.random() * 1000).toString();
